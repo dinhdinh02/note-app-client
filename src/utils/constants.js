9bf53a331567e7b9cae1d531058ac4bf6b6e423e
dinhdinh02 <dinhn1182@gmail.com>
2024-05-09 12:20:54 +0700

    update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const GRAPHQL_SERVER = 'http://localhost:4000'; //https://note-app-server-eyaw.onrender.com
+export const GRAPHQL_SERVER = 'https://note-app-server-eyaw.onrender.com'; //localhost:4000
 export const GRAPHQL_SUBSCRIPTION_ENDPOINT = 'wss://note-app-holetex-server.onrender.com/graphql';
